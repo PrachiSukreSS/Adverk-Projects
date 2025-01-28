16c605b78d5883dd2a98b48988597bf91b234493
Prachi Sukre <prachisukre2005@gmail.com>
2025-01-28 22:19:32 +0530

    Initial commit

---
+++ Minor/script.js
@@ -0,0 +1,10 @@
+function validateForm() {
+  let name = document.forms["contactForm"]["name"].value;
+  let email = document.forms["contactForm"]["email"].value;
+  let message = document.forms["contactForm"]["message"].value;
+
+  if (name == "" || email == "" || message == "") {
+    alert("All fields must be filled out!");
+    return false;
+  }
+}
